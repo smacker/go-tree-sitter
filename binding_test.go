31b8ee725e220e877a3e4e16648689a637edb21c
Maxim Sukharev <maxim@sourced.tech>
2018-02-12 23:49:44 +0100

    add more methods to node

---
+++ binding_test.go
@@ -0,0 +1,37 @@
+package sitter_test
+
+import (
+	"testing"
+
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRootNode(t *testing.T) {
+	assert := assert.New(t)
+
+	n := sitter.Parse([]byte("let a = 1"), "javascript")
+
+	assert.Equal(uint32(0), n.StartByte())
+	assert.Equal(uint32(9), n.EndByte())
+	assert.Equal("(program (lexical_declaration (variable_declarator (identifier) (number))))", n.String())
+	assert.Equal("program", n.Type())
+
+	assert.Equal(true, n.IsNamed())
+	assert.Equal(false, n.IsMissing())
+	assert.Equal(false, n.HasChanges())
+	assert.Equal(false, n.HasError())
+
+	assert.Equal(uint32(1), n.ChildCount())
+	assert.Equal(uint32(1), n.NamedChildCount())
+	assert.Equal(^uint32(0), n.ChildIndex())
+
+	assert.Nil(n.Parent())
+	assert.Nil(n.NextSibling())
+	assert.Nil(n.NextNamedSibling())
+	assert.Nil(n.PrevSibling())
+	assert.Nil(n.PrevNamedSibling())
+
+	assert.NotNil(n.Child(0))
+	assert.NotNil(n.NamedChild(0))
+}
--- bindings.go
+++ bindings.go
@@ -1,39 +1,9 @@
 package sitter
 
 //#cgo CFLAGS: -g -I${SRCDIR}/tree-sitter/include
-//#cgo LDFLAGS: ${SRCDIR}/tree-sitter/out/Release/obj.target/libruntime.a
+//#cgo LDFLAGS: ${SRCDIR}/tree-sitter/out/Release/libruntime.a
 //#include "bindings.h"
 import "C"
-
-type Node struct {
-	cNode C.struct___0
-	cDoc  *C.struct_TSDocument
-}
-
-func (n *Node) String() string {
-	return C.GoString(C.ts_node_string(n.cNode, n.cDoc))
-}
-
-func (n *Node) Type() string {
-	return C.GoString(C.ts_node_type(n.cNode, n.cDoc))
-}
-
-func (n *Node) Children() int {
-	return int(C.ts_node_child_count(n.cNode))
-}
-
-func (n *Node) Name() string {
-	if n.Type() != "function" {
-		return ""
-	}
-	nNode := C.ts_node_named_child(n.cNode, C.uint32_t(0))
-	return C.GoString(C.readValue(n.cDoc, nNode))
-}
-
-func (n *Node) Child(idx int) *Node {
-	cNode := C.ts_node_child(n.cNode, C.uint32_t(idx))
-	return &Node{cNode, n.cDoc}
-}
 
 func Parse(content []byte, lang string) *Node {
 	input := (*C.char)(C.CBytes(content))
@@ -45,4 +15,118 @@ func Parse(content []byte, lang string) *Node {
 	default:
 		panic("language not found")
 	}
+}
+
+type Node struct {
+	cNode C.struct___0
+	cDoc  *C.struct_TSDocument
+}
+
+func (n *Node) StartByte() uint32 {
+	return uint32(C.ts_node_start_byte(n.cNode))
+}
+
+func (n *Node) EndByte() uint32 {
+	return uint32(C.ts_node_end_byte(n.cNode))
+}
+
+func (n *Node) String() string {
+	return C.GoString(C.ts_node_string(n.cNode, n.cDoc))
+}
+
+func (n *Node) Type() string {
+	return C.GoString(C.ts_node_type(n.cNode, n.cDoc))
+}
+
+func (n *Node) IsNamed() bool {
+	return bool(C.ts_node_is_named(n.cNode))
+}
+
+func (n *Node) IsMissing() bool {
+	return bool(C.ts_node_is_missing(n.cNode))
+}
+
+func (n *Node) HasChanges() bool {
+	return bool(C.ts_node_has_changes(n.cNode))
+}
+
+func (n *Node) HasError() bool {
+	return bool(C.ts_node_has_error(n.cNode))
+}
+
+func (n *Node) Parent() *Node {
+	cNode := C.ts_node_parent(n.cNode)
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+func (n *Node) Child(idx int) *Node {
+	cNode := C.ts_node_child(n.cNode, C.uint32_t(idx))
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+func (n *Node) NamedChild(idx int) *Node {
+	cNode := C.ts_node_named_child(n.cNode, C.uint32_t(idx))
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+func (n *Node) ChildCount() uint32 {
+	return uint32(C.ts_node_child_count(n.cNode))
+}
+
+func (n *Node) NamedChildCount() uint32 {
+	return uint32(C.ts_node_named_child_count(n.cNode))
+}
+
+func (n *Node) ChildIndex() uint32 {
+	return uint32(C.ts_node_child_index(n.cNode))
+}
+
+func (n *Node) NextSibling() *Node {
+	cNode := C.ts_node_next_sibling(n.cNode)
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+func (n *Node) NextNamedSibling() *Node {
+	cNode := C.ts_node_next_named_sibling(n.cNode)
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+func (n *Node) PrevSibling() *Node {
+	cNode := C.ts_node_prev_sibling(n.cNode)
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+func (n *Node) PrevNamedSibling() *Node {
+	cNode := C.ts_node_prev_named_sibling(n.cNode)
+	if cNode.data == nil {
+		return nil
+	}
+	return &Node{cNode, n.cDoc}
+}
+
+// need love
+func (n *Node) Name() string {
+	if n.Type() != "function" {
+		return ""
+	}
+	nNode := C.ts_node_named_child(n.cNode, C.uint32_t(0))
+	return C.GoString(C.readValue(n.cDoc, nNode))
 }
