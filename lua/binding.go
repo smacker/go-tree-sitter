a79223a111631a7e83e3c673e56f3c12120b1dc4
Máximo Cuadros <mcuadros@gmail.com>
2020-02-19 10:04:04 +0100

    add lua support

---
+++ lua/binding.go
@@ -0,0 +1,15 @@
+package lua
+
+//#include "parser.h"
+//TSLanguage *tree_sitter_lua();
+import "C"
+import (
+	"unsafe"
+
+	sitter "github.com/smacker/go-tree-sitter"
+)
+
+func GetLanguage() *sitter.Language {
+	ptr := unsafe.Pointer(C.tree_sitter_lua())
+	return sitter.NewLanguage(ptr)
+}
