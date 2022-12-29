0adde312c2f2aa83ea846c08c3fd5b2e24c91144
Alex Vazquez <alex@codepen.io>
2022-12-29 12:08:34 -0800

    Handle duplicate symbol errors caused by externing same function names (#5)
    * Handle duplicate symbol error caused by externing the same global
    functions. Just commented out duplicate functions

--- vue/tree_sitter_html/scanner.cc
+++ vue/tree_sitter_html/scanner.cc
@@ -280,31 +280,33 @@ struct Scanner {
 
 }
 
-extern "C" {
+// Commented out due to conflict with HTML grammar
+// Error: duplicate symbol '_tree_sitter_html_external_scanner_destroy' in:
+// extern "C" {
 
-void *tree_sitter_html_external_scanner_create() {
-  return new Scanner();
-}
+// void *tree_sitter_html_external_scanner_create() {
+//   return new Scanner();
+// }
 
-bool tree_sitter_html_external_scanner_scan(void *payload, TSLexer *lexer,
-                                            const bool *valid_symbols) {
-  Scanner *scanner = static_cast<Scanner *>(payload);
-  return scanner->scan(lexer, valid_symbols);
-}
+// bool tree_sitter_html_external_scanner_scan(void *payload, TSLexer *lexer,
+//                                             const bool *valid_symbols) {
+//   Scanner *scanner = static_cast<Scanner *>(payload);
+//   return scanner->scan(lexer, valid_symbols);
+// }
 
-unsigned tree_sitter_html_external_scanner_serialize(void *payload, char *buffer) {
-  Scanner *scanner = static_cast<Scanner *>(payload);
-  return scanner->serialize(buffer);
-}
+// unsigned tree_sitter_html_external_scanner_serialize(void *payload, char *buffer) {
+//   Scanner *scanner = static_cast<Scanner *>(payload);
+//   return scanner->serialize(buffer);
+// }
 
-void tree_sitter_html_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
-  Scanner *scanner = static_cast<Scanner *>(payload);
-  scanner->deserialize(buffer, length);
-}
+// void tree_sitter_html_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
+//   Scanner *scanner = static_cast<Scanner *>(payload);
+//   scanner->deserialize(buffer, length);
+// }
 
-void tree_sitter_html_external_scanner_destroy(void *payload) {
-  Scanner *scanner = static_cast<Scanner *>(payload);
-  delete scanner;
-}
+// void tree_sitter_html_external_scanner_destroy(void *payload) {
+//   Scanner *scanner = static_cast<Scanner *>(payload);
+//   delete scanner;
+// }
 
-}
+// }
