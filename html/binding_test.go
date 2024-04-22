3e85dc0983b850c02b6c64d75bd9bea8947eeee0
Mick Grove <mgrove@gmail.com>
2024-04-22 09:02:32 -0700

    updated HTML grammar to v0.20.2

--- html/binding_test.go
+++ html/binding_test.go
@@ -15,7 +15,7 @@ func TestGrammar(t *testing.T) {
 	n, err := sitter.ParseCtx(context.Background(), []byte(`<HTML><BODY><P>Hello World</P></BODY></HTML>`), html.GetLanguage())
 	assert.NoError(err)
 	assert.Equal(
-		"(fragment (element (start_tag (tag_name)) (element (start_tag (tag_name)) (element (start_tag (tag_name)) (text) (end_tag (tag_name))) (end_tag (tag_name))) (end_tag (tag_name))))",
+		"(document (element (start_tag (tag_name)) (element (start_tag (tag_name)) (element (start_tag (tag_name)) (text) (end_tag (tag_name))) (end_tag (tag_name))) (end_tag (tag_name))))",
 		n.String(),
 	)
 }
