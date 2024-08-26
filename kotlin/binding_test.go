a46cd22a0121b68f60737af433d208c82ce382d4
Arturo Salvio <775410+asalvi0@users.noreply.github.com>
2024-08-26 13:04:41 -0600

    updated kotlin test

--- kotlin/binding_test.go
+++ kotlin/binding_test.go
@@ -20,10 +20,10 @@ fun main(args: Array<String>) {
 }
 `
 
-var output = `(source_file (import_list (import_header (identifier (simple_identifier) (simple_identifier) (simple_identifier)))) (function_declaration (simple_identifier) (function_value_parameters (parameter (simple_identifier) (user_type (type_identifier) (type_arguments (type_projection (user_type (type_identifier))))))) (function_body (statements (property_declaration (variable_declaration (simple_identifier)) (call_expression (navigation_expression (simple_identifier) (navigation_suffix (simple_identifier))) (call_suffix (value_arguments)))) (call_expression (simple_identifier) (call_suffix (value_arguments (value_argument (string_literal (interpolated_identifier))))))))))`
+var output = `(source_file (import_list (import_header (identifier (simple_identifier) (simple_identifier) (simple_identifier)))) (function_declaration (simple_identifier) (function_value_parameters (parameter (simple_identifier) (user_type (type_identifier) (type_arguments (type_projection (user_type (type_identifier))))))) (function_body (statements (property_declaration (binding_pattern_kind) (variable_declaration (simple_identifier)) (call_expression (navigation_expression (simple_identifier) (navigation_suffix (simple_identifier))) (call_suffix (value_arguments)))) (call_expression (simple_identifier) (call_suffix (value_arguments (value_argument (string_literal (string_content) (interpolated_identifier))))))))))`
 
 func TestGrammar(t *testing.T) {
 	n, err := sitter.ParseCtx(context.Background(), []byte(ktCode), kotlin.GetLanguage())
 	assert.Nil(t, err)
-	assert.Equal(t, n.String(), output)
+	assert.Equal(t, output, n.String())
 }
